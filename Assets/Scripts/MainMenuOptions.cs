a92b01bfb4706f4ba1ad56a39d4488d1507616ed
Manu <black_falcon00@hotmail.com>
2018-12-22 14:28:59 +0100

    Episode 23

---
+++ Assets/Scripts/MainMenuOptions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuOptions : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void PlayGame() {
+		SceneManager.LoadScene (0);
+	}
+
+	public void Credits() {
+		SceneManager.LoadScene (2);
+	}
+}
