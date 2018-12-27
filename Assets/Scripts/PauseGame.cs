5137758b1ea8dbbc12c55ca54c0b184d8a2bf4c9
Manu <black_falcon00@hotmail.com>
2018-12-27 16:26:56 +0100

    Episodes 30-31 Improve pause menu, barrel explosion.

--- Assets/Scripts/PauseGame.cs
+++ Assets/Scripts/PauseGame.cs
@@ -2,32 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
+using System.IO;
 
 public class PauseGame : MonoBehaviour {
 
 	public FirstPersonController ThePlayer;
+	public GameObject PauseMenu;
 
 	private bool paused = false;
 
+
 	// Use this for initialization
 	void Start () {
-		
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown ("Pause")) {
 			if (!paused) {
+				Cursor.lockState = CursorLockMode.None;
+				Cursor.visible = true;
 				Time.timeScale = 0.0f;
 				paused = true;
 				ThePlayer.enabled = false;
-				Cursor.visible = true;
+				PauseMenu.SetActive(true);
 			} else {
-				Time.timeScale = 1.0f;
-				paused = false;
-				ThePlayer.enabled = true;
-				Cursor.visible = false;
+				ResumeGame ();
 			}
 		}
+	}
+
+	public void ResumeGame() {
+		Cursor.visible = false;
+		Time.timeScale = 1.0f;
+		paused = false;
+		ThePlayer.enabled = true;
+		PauseMenu.SetActive(false);
 	}
 }
