f8c7762d4ff4d441e3620684e98a24022aaf1aa7
Manu <black_falcon00@hotmail.com>
2019-01-01 14:45:58 +0100

    Episode 38: zoom anim for the scope.

--- Assets/Scripts/SniperScope.cs
+++ Assets/Scripts/SniperScope.cs
@@ -8,23 +8,41 @@ public class SniperScope : MonoBehaviour {
 	public Camera FPCCam;
 	public GameObject Cursor;
 	public GameObject SniperScopeTex;
+	public int fieldView = 60;
+
+	private bool zoomingIn = false;
+	private WaitForSeconds wfsZoomTempo;
 
 	// Use this for initialization
 	void Start () {
-		
+		wfsZoomTempo = new WaitForSeconds (0.01f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonDown (1)) {
-			FPCCam.fieldOfView = 20;
-			Cursor.SetActive(false);
-			SniperScopeTex.SetActive (true);
+			if (!zoomingIn) {
+				Cursor.SetActive (false);
+				SniperScopeTex.SetActive (true);
+				StartCoroutine ("ZoomingCam");
+				zoomingIn = true;
+			}
 		}
 		if (Input.GetMouseButtonUp (1)) {
-			FPCCam.fieldOfView = 60;
+			StopAllCoroutines ();
+			fieldView = 60;
+			FPCCam.fieldOfView = fieldView;
 			Cursor.SetActive(true);
 			SniperScopeTex.SetActive (false);
+			zoomingIn = false;
+		}
+	}
+
+	IEnumerator ZoomingCam() {
+		while (fieldView > 25) {
+			FPCCam.fieldOfView = fieldView;
+			fieldView -= 2;
+			yield return wfsZoomTempo;
 		}
 	}
 }
